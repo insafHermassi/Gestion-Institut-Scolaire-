650cf6f590a71a590d635d7e61678bd862aa3cbd
insafHermassi <insafhermassi27@gmail.com>
2023-03-16 15:27:00 +0100

    create insitute project

---
+++ src/main/java/com/NSTGroupe/institut/InstitutApplication.java
@@ -0,0 +1,13 @@
+package com.NSTGroupe.institut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstitutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstitutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/NSTGroupe/institut/InstitutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NSTGroupe.institut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstitutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
